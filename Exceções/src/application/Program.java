5995c09dc6c07dd77d6b3c154a0d7c15d1a57504
Igor Cortez <igor.barbosa14@gmail.com>
2019-05-28 11:00:01 -0300

    Stack trace

--- Exceções/src/application/Program.java
+++ Exceções/src/application/Program.java
@@ -4,26 +4,41 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
-	public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
 	
-	try {
-	System.out.println("Digite os nomes para serem armazenados no array");
-	String[] vect = sc.nextLine().split(" ");
-	System.out.println("Digite a posição que deseja acessar no array criado:");
-	int index = sc.nextInt();
-	
-	System.out.println(vect[index]);
+	public static void method1() {
+		System.out.println("*** START METHOD1 ***");
+		method2();
+		System.out.println("*** START METHOD1 ***");
 	}
-	catch(ArrayIndexOutOfBoundsException e){
+	
+	public static void method2() {
 		
-		System.out.println("Invalid Position!!!!");
-	}
-	catch(InputMismatchException e) {
-		System.out.println("Input Error!!");
-	}
-	System.out.println("End of Program!!!");
-	
-	sc.close();
-	}
+		System.out.println("*** START METHOD2 ***");
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+		System.out.println("Digite os nomes para serem armazenados no array");
+		String[] vect = sc.nextLine().split(" ");
+		System.out.println("Digite a posição que deseja acessar no array criado:");
+		int index = sc.nextInt();
+		
+		System.out.println(vect[index]);
+		}
+		catch(ArrayIndexOutOfBoundsException e){
+			
+			System.out.println("Invalid Position!!!!");
+			e.printStackTrace();
+			sc.hasNext();
+		}
+		catch(InputMismatchException e) {
+			System.out.println("Input Error!!");
+		}
+		
+		sc.close();
+		System.out.println("*** END METHOD2 ***");
+		}
+	public static void main(String[] args) {
+		method1();
+		
+		}
 }
